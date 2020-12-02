e66ace0290313b4ac586de9405442b682213c84e
Nitin Sijwali <sijwali@Nitins-MacBook-Air.local>
2020-12-02 15:47:17 +0530

    initial commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,49 +1,50 @@
-import React, {Component} from 'react';
-import {usersJSON} from '../data';
+import React, { Component } from 'react';
+import { usersJSON } from '../data';
 
 export default class Table extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			users: usersJSON
-		}
+			users: usersJSON,
+		};
 	}
 
-	componentWillReceiveProps() {
-		
-	}
+	componentWillReceiveProps() {}
 
-    // complete the comparators
-	compareByAge(a, b) {
-		
-	}
+	// complete the comparators
+	compareByAge(a, b) {}
 
-	compareByName(a, b) {
-		
-	}
+	compareByName(a, b) {}
 
-	compareByPoints(a, b) {
-		
-	}
+	compareByPoints(a, b) {}
 
-	compareByRank(a, b) {
-		
-	}
+	compareByRank(a, b) {}
 
 	render() {
- 
-		return (<div>
-			<table className="table table-striped">
-				<thead>
-					<tr key="head">
-						<th>Age</th>
-						<th>Name</th>
-						<th>Points</th>
-						<th>Rank</th>
-					</tr>
-				</thead>
-				<tbody></tbody>
-			</table>
-		</div>)
+		console.table(this.state.users);
+		return (
+			<div>
+				<table className='table table-striped'>
+					<thead>
+						<tr key='head'>
+							<th>Age</th>
+							<th>Name</th>
+							<th>Points</th>
+							<th>Rank</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.users.map(({ age, name, points, rank }) => (
+							<tr>
+								<td>{age}</td>
+								<td>{name}</td>
+								<td>{points}</td>
+								<td>{rank}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		);
 	}
 }
